993c41b5aee52b689d7e09733ae3a4739367a35e
abtorres <abtorres079@gmail.com>
2019-08-22 20:22:35 -0600

    add header

---
+++ luncher/src/components/AppRouter/AppRouter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+const AppRouter = () => {
+    <Switch>
+        <Route exact path='/' component={} />
+        <Route path='' component={} />
+        <Route path='/' component={} />
+    </Switch>
+}
